bc696b6ff701ec289ef64b81947b232d10af696e
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-05 02:51:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,6 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, holiday|
-  holiday.each do |supply|
 end
 
 def all_holidays_with_bbq(holiday_hash)
